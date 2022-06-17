5c80620e7cac63636b66cf5ced50cb66c79ca74c
splotycode <davidscandurra@gmail.com>
2022-06-18 01:31:36 +0200

    Add dummy implementation

---
+++ src/main/java/io/github/splotycode/stackgraph/internal/graph/DummyStackGraph.java
@@ -0,0 +1,13 @@
+package io.github.splotycode.stackgraph.internal.graph;
+
+import io.github.splotycode.stackgraph.StackGraph;
+
+/**
+ * @author David (_Esel)
+ */
+public class DummyStackGraph implements StackGraph {
+    @Override
+    public void methodEnter(String information) {
+
+    }
+}
